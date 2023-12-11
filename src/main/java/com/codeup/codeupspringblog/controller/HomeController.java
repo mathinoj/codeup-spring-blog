6cb8d55d1193cb1f7ad0aed6f41a87f391903de7
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-12-11 09:31:08 -0600

    did multiply and divide get mapping

--- src/main/java/com/codeup/codeupspringblog/controller/HomeController.java
+++ src/main/java/com/codeup/codeupspringblog/controller/HomeController.java
@@ -22,10 +22,21 @@ public class HomeController {
         return "Your numbs added = " + (numb1 + numb2);
     }
 
-
-    @GetMapping(path = "/subtract/{numb1}/and/{numb2}", produces = "application/json")
+    @GetMapping(path = "/subtract/{numb1}/from/{numb2}", produces = "application/json")
     @ResponseBody
     public String subNumbs(@PathVariable int numb1, @PathVariable int numb2) {
         return "Your numbs subtracted = " + (numb2 - numb1);
+    }
+
+    @GetMapping(path = "/times/{numb1}/and/{numb2}", produces = "application/json")
+    @ResponseBody
+    public String subTimes(@PathVariable int numb1, @PathVariable int numb2) {
+        return "Your numbs subtracted = " + (numb1 * numb2);
+    }
+
+    @GetMapping(path = "/divide/{numb1}/by/{numb2}", produces = "application/json")
+    @ResponseBody
+    public String subDivide(@PathVariable int numb1, @PathVariable int numb2) {
+        return "Your numbs subtracted = " + (numb1 / numb2);
     }
 }
