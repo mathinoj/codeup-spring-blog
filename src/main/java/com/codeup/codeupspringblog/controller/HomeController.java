ff4573a4292e7cb93fe25dd1b2c0267859cbd7d5
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-12-11 09:28:28 -0600

    did add and subtraction get mapping

--- src/main/java/com/codeup/codeupspringblog/controller/HomeController.java
+++ src/main/java/com/codeup/codeupspringblog/controller/HomeController.java
@@ -2,7 +2,10 @@ package com.codeup.codeupspringblog.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class HomeController {
@@ -13,4 +16,16 @@ public class HomeController {
         return "This is the landing page!"; //whatever function is returned will be used by the requester response body
     }
 
+    @GetMapping(path = "/add/{numb1}/and/{numb2}", produces = "application/json")
+    @ResponseBody
+    public String addNumbs(@PathVariable int numb1, @PathVariable int numb2) {
+        return "Your numbs added = " + (numb1 + numb2);
+    }
+
+
+    @GetMapping(path = "/subtract/{numb1}/and/{numb2}", produces = "application/json")
+    @ResponseBody
+    public String subNumbs(@PathVariable int numb1, @PathVariable int numb2) {
+        return "Your numbs subtracted = " + (numb2 - numb1);
+    }
 }
